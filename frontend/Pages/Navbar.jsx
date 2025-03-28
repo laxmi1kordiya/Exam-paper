e663c3eee8f26b9292a24d11ef8968d5b38f50ae
laxmi1kordiya <laxmikordiya544@gmail.com>
2025-03-28 17:41:23 +0530

    remove login button

--- frontend/Pages/Navbar.jsx
+++ frontend/Pages/Navbar.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { navLinks } from "../Assets/Mocks/navLink.mock"; // Importing mock data
-import SignUp from "./SignUp";
+import { navLinks } from "../Assets/Mocks/navLink.mock"; 
 
 const Navbar = () => {
   return (
     <nav className="header">
-      {/* Logo */}
       <div className="logo">
         <img src="logo.png" alt="Logo" />
       </div>
-
-      {/* Navigation Links */}
       <ul className="nav">
         {navLinks.map((item) => (
           <li key={item.id}>
@@ -19,11 +15,6 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
-
-      {/* Login Button */}
-      <Link to="/login">
-        <button className="login-btn">Login</button>
-      </Link>
         <Link to="/SignUp">
          <button className="login-btn" >Register</button> 
         </Link>
