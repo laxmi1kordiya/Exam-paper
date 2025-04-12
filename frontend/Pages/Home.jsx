eff15a4d781b963a4c4a7d41ac81086d340e9c09
laxmi1kordiya <laxmikordiya544@gmail.com>
2025-04-12 23:22:51 +0530

    add iphoneImage with using index file

--- frontend/Pages/Home.jsx
+++ frontend/Pages/Home.jsx
@@ -1,31 +1,34 @@
 import React from "react";
-import { motion } from 'framer-motion'
 import Features from "./Features";
 import Price from "./Price";
 import AboutUs from "./AboutUs";
 import Footer from "./Footer";
 import HowToUse from "./HowToUse";
 import Navbar from "./Navbar";
+import { iphoneImage } from "../Assets";
 
 const Home = () => {
-  return(
+  return (
     <>
-    <Navbar />
-    <section className="section1">
-    <div>
-        <span>Generate Exams Papers For GSEB Board</span>
-        <span>We Provide users to generate exam paper with many plans. you can also register as Teachers , Students , Schools ,  and Classes.</span>
-    </div>
-    <div className="img">
-        <img src="frontend/Assets/images/iphone.png" alt="Error"/>
-    </div>
-</section>
-       <Features />
-       <Price />
-       <HowToUse />
-       <AboutUs />
-       <Footer />
-          </>
+      <Navbar />
+      <section className="section1">
+        <div>
+          <span>Generate Exams Papers For GSEB Board</span>
+          <span>
+            We Provide users to generate exam paper with many plans. you can
+            also register as Teachers , Students , Schools , and Classes.
+          </span>
+        </div>
+        <div className="img">
+          <img src={iphoneImage} alt="Error" />
+        </div>
+      </section>
+      <Features />
+      <Price />
+      <HowToUse />
+      <AboutUs />
+      <Footer />
+    </>
   );
 };
 export default Home;
