e65b9cc3b04a214b2b66ef6398d84b60654af1c5
laxmi1kordiya <laxmikordiya544@gmail.com>
2025-05-01 17:36:08 +0530

    remove access for dist

--- dist/client/assets/NavigationMenu-9b815394.js
+++
@@ -1,2 +0,0 @@
-import{u as a}from"./index-97231a38.js";const e=()=>a();export{e as n};
-//# sourceMappingURL=NavigationMenu-9b815394.js.map
--- dist/client/assets/NavigationMenu-df044dd9.js
+++
@@ -1,2 +0,0 @@
-import{u as a}from"./index-9ba0293e.js";const e=()=>a();export{e as n};
-//# sourceMappingURL=NavigationMenu-df044dd9.js.map
