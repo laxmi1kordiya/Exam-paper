c4aec4d08bdaee1ecba55be260bf8130717dde42
yash <yashkoradiya011@gmail.com>
2025-05-02 18:00:27 +0530

    updated generatepaper and MyPapers

--- frontend/Pages/admin/MyPapers.jsx
+++ frontend/Pages/admin/MyPapers.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 
 const MyPapers = () => {
-    return(
-        <div className="content-page">
+  return (
+    <div className="content-page">
       <div className="main-content">
+        <div className="attention">
+          <b>Attention:</b>
+          <br></br>
+          <span className="warning">
+            1.The generated paper will be saved for 30 days.
+          </span>
+          <br></br>
+          <span className="warning">
+            2.You can edit the paper within 7 days of creation.
+          </span>
+          <br></br>
         </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 };
 
 export default MyPapers;
