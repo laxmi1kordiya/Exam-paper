cc9cf276be9f0018d630ff996338971ab786a100
laxmi1kordiya <laxmikordiya544@gmail.com>
2025-05-04 14:02:27 +0530

    add attention banner

--- frontend/Pages/admin/MyPapers.jsx
+++ frontend/Pages/admin/MyPapers.jsx
@@ -30,67 +30,72 @@ const MyPapers = () => {
     }
   };
   return (
-    <div className="content-page">
-      <div className="main-content">
-        <div className="attention">
-          <b>Attention:</b>
-          <br></br>
-          <span className="warning">
-            1.The generated paper will be saved for 30 days.
-          </span>
-          <br></br>
-          <span className="warning">
-            2.You can edit the paper within 7 days of creation.
-          </span>
-          <br></br>
+    <>
+      <div className="banner">
+        <div className="main-content">
+          <div className="attention">
+            <b>Attention:</b>
+            <br></br>
+            <span className="warning">
+              1.The generated paper will be saved for 30 days.
+            </span>
+            <br></br>
+            <span className="warning">
+              2.You can edit the paper within 7 days of creation.
+            </span>
+            <br></br>
+          </div>
         </div>
-
-        <table>
-          <tbody>
-            {myPapers.map((paper) => (
-              <tr key={paper._id}>
-                <td>
-                  {`${paper?.paperSetting?.board} >> ${paper?.paperSetting?.standard} >> ${paper?.paperSetting?.subject}`}
-                </td>
-
-                <td>
-                  {new Date(paper.created).toLocaleDateString("en-US", {
-                    day: "2-digit",
-                    month: "short",
-                    year: "numeric",
-                  })}
-                </td>
-                <td>
-                  <GeneratePDF
-                    formData={paper?.paperSetting?.formData}
-                    allData={paper?.paperSetting?.allData}
-                    selectedQuestions={
-                      paper?.paperSetting?.selectedQuestionsArray
-                    }
-                    headerData={headerData}
-                  />
-                  <GenerateAnsKey
-                    formData={paper?.paperSetting?.formData}
-                    allData={paper?.paperSetting?.allData}
-                    selectedQuestions={
-                      paper?.paperSetting?.selectedQuestionsArray
-                    }
-                    headerData={headerData}
-                  />
-                  <button
-                    onClick={() => {
-                      deletePaperData(paper._id);
-                    }}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
       </div>
-    </div>
+      <div className="content-page">
+        <div className="main-content">
+          <table>
+            <tbody>
+              {myPapers.map((paper) => (
+                <tr key={paper._id}>
+                  <td>
+                    {`${paper?.paperSetting?.board} >> ${paper?.paperSetting?.standard} >> ${paper?.paperSetting?.subject}`}
+                  </td>
+
+                  <td>
+                    {new Date(paper.created).toLocaleDateString("en-US", {
+                      day: "2-digit",
+                      month: "short",
+                      year: "numeric",
+                    })}
+                  </td>
+                  <td>
+                    <GeneratePDF
+                      formData={paper?.paperSetting?.formData}
+                      allData={paper?.paperSetting?.allData}
+                      selectedQuestions={
+                        paper?.paperSetting?.selectedQuestionsArray
+                      }
+                      headerData={headerData}
+                    />
+                    <GenerateAnsKey
+                      formData={paper?.paperSetting?.formData}
+                      allData={paper?.paperSetting?.allData}
+                      selectedQuestions={
+                        paper?.paperSetting?.selectedQuestionsArray
+                      }
+                      headerData={headerData}
+                    />
+                    <button
+                      onClick={() => {
+                        deletePaperData(paper._id);
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
   );
 };
 
