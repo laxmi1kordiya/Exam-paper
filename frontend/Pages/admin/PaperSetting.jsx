9d6fb6f13503be5e07d6ffae60c568c70e8c2872
laxmi1kordiya <laxmikordiya544@gmail.com>
2025-05-10 10:39:00 +0530

    update paper setting and add WaterMarkTaxt

--- frontend/Pages/admin/PaperSetting.jsx
+++ frontend/Pages/admin/PaperSetting.jsx
@@ -1,92 +1,80 @@
-import React, { useEffect, useState, useCallback } from "react";
-import { useAuthenticatedFetch } from "../../Api/Axios";
-import { toast, ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { navigate } from "../../Components/NavigationMenu";
+import React from "react";
 
-const PaperSetting = () => {
-  const fetch = useAuthenticatedFetch();
-  const setNavigate = navigate();
-
-  const [formData, setFormData] = useState({
-    title: "Create Paper",
-    subtitle: "Paper Generate by Create Paper",
-    // WaterMark: false,
-    // WaterMarkTaxt: "",
-  });
-
-  const fetchData = useCallback(async () => {
-    const res = await fetch.get("getHeaderData");
-    const data = res?.data;
-    if (data) {
-      setFormData(data);
-    }
-  }, []);
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log(formData, "formData");
-    formData["userId"] = localStorage.getItem("userId");
-    const res1 = await fetch.post("paperSetting", formData);
-    console.log(res1, "res1");
-    toast.success("Paper header updated successfully!");
-    if (res1) {
-      setNavigate("/admin/my-papers");
-    }
-  };
-
+const PaperSetting = ({ handleNextFromStep2, headerData, updateHeader }) => {
   return (
-    <div className="content-page">
-      <div className="main-content">
-        <div className="signin-box">
-          <h3 className="text-center">Paper Header Settings</h3>
-          <p className="text-center text-dark">
-            Configure details for the exam paper
-          </p>
-          <form onSubmit={handleSubmit}>
-            <div className="row">
-              <div className="form-group">
-                <label htmlFor="title">Title</label>
-                <input
-                  type="text"
-                  name="title"
-                  className="form-control"
-                  id="title"
-                  value={formData.title}
-                  onChange={handleChange}
-                />
-              </div>
-
-              <div className="form-group">
-                <label htmlFor="subtitle">Subtitle</label>
-                <input
-                  type="text"
-                  name="subtitle"
-                  className="form-control"
-                  id="subtitle"
-                  value={formData.subtitle}
-                  onChange={handleChange}
-                />
-              </div>
-            </div>
-
-            <button type="submit">Submit</button>
-          </form>
+    <>
+      <div className="header">
+        <div className="title-container">
+          <h2>Customize Exam Paper Header</h2>
+          <p>Set the title and subtitle for your exam paper.</p>
         </div>
       </div>
-      <ToastContainer />
-    </div>
+      <form onSubmit={handleNextFromStep2}>
+        <div className="form-container">
+          <div className="form-group">
+            <label htmlFor="title">Institute Name</label>
+            <input
+              type="text"
+              name="title"
+              className="form-control"
+              id="title"
+              value={headerData.title}
+              onChange={updateHeader}
+              placeholder="Enter paper title"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="subtitle">Subtitle</label>
+            <input
+              type="text"
+              name="subtitle"
+              className="form-control"
+              id="subtitle"
+              value={headerData.subtitle}
+              onChange={updateHeader}
+              placeholder="Enter subtitle"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="paperTime">Paper Time (in minute)</label>
+            <input
+              type="number"
+              name="paperTime"
+              className="form-control"
+              id="paperTime"
+              value={headerData.paperTime}
+              onChange={updateHeader}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="paperTime">Water Mark</label>
+            <input
+              type="checkbox"
+              name="WaterMark"
+              className="form-control"
+              id="WaterMark"
+              value={headerData.WaterMark}
+              onChange={updateHeader}
+            />
+          </div>
+          <div className="form-group">
+            {headerData.WaterMark && (
+              <>
+                <label htmlFor="paperTime">Water Mark Text</label>
+                <input
+                  type="text"
+                  name="WaterMarkTaxt"
+                  className="form-control"
+                  id="WaterMarkTaxt"
+                  value={headerData.WaterMarkTaxt}
+                  onChange={updateHeader}
+                />
+              </>
+            )}
+          </div>
+        </div>
+      </form>
+    </>
   );
 };
 
