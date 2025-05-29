18e5df3c1c5c7b8ef225dd94281a8fce0a7e145e
laxmi1kordiya <laxmikordiya544@gmail.com>
2025-05-29 19:05:09 +0530

    update cron job

--- Server/backend/Controllers/cronJobs.js
+++ Server/backend/Controllers/cronJobs.js
@@ -1,37 +1,24 @@
-import { schedule } from 'node-cron';
-import { deleteExpiredPapers } from '../Model/common.js';
+import { deleteMany, find } from "../Model/common.js";
 
-const setupCronJobs = () => {
-  // Cron job to delete papers older than 30 days
-  schedule('0 15 * * *', async () => {
-    console.log('Running 30-day paper deletion cron job...');
-    // Calculate 30 days in milliseconds
-    const thirtyDaysInMs = 30 * 24 * 60 * 60 * 1000;
+const setupCronJobs = async () => {
+  console.log("Cron jobs scheduled.");
 
-    try {
-      // Pass the NUMBER (milliseconds) to deleteExpiredPapers
-      const result = await deleteExpiredPapers(thirtyDaysInMs);
-      console.log(`${result.deletedCount} papers deleted older than 30 days.`);
-    } catch (error) {
-      console.error('Error deleting 30-day old papers:', error);
+  try {
+    const papers = await find("paper", {});
+    const thirtyDaysAgo = new Date();
+    thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
+
+    for (let i = 0; i < papers.length; i++) {
+      const element = papers[i];
+      const createdDate = new Date(element.created);
+      if (createdDate <= thirtyDaysAgo) {
+        await deleteMany("paper", { _id: element._id });
+        console.log(`Deleted paper created on ${createdDate}`);
+      }
     }
-  });
-
-  schedule('*/5 * * * * *', async () => {
-    console.log('Running 2-minute paper deletion (testing) cron job...');
-    const twoMinutesInMs = 2 * 60 * 1000;
-
-    try {
-      const result = await deleteExpiredPapers(twoMinutesInMs);
-    } catch (error) {
-      console.error('Error deleting 2-minute old papers (testing):', error);
-    }
-  });
-
-  console.log('Cron jobs scheduled.');
+  } catch (error) {
+    console.error("Error in cron job:", error);
+    throw error;
+  }
 };
-
-
-setupCronJobs();
-
 export default setupCronJobs;
